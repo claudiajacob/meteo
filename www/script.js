94fc124664a9ce1561a856474c8b535e005d9901
claudiajacob <73claudiajacob@gmail.com>
2022-01-14 19:10:45 +0100

    Ajout commentaire js

--- www/script.js
+++ www/script.js
@@ -1,4 +1,4 @@
-let API_KEY ='2bd5dddb9287492f812f4e079c44f065';
+let API_KEY ='2bd5dddb9287492f812f4e079c44f065'; //Clef de l'API
 let ville = "Chambery"; //Ville de base
 var infos = []; //Variable ou sera stocké les informations renvoyé par l'API
 
@@ -22,7 +22,7 @@ function meteoPush(n){
      var donnes =JSON.parse(n); // On transforme le texte recu par l'API en objet JSON
      donnes = donnes.data;      // On récupere uniquement les "data"
      donnes= donnes[0];         // 
-     console.log(donnes);
+    //console.log(donnes);      //Debugage
     changementFond(donnes.temp);
     affichageVille(donnes.city_name);   //On change l'affichage de la ville
     changementVariables(donnes);        //On change les données secondaires
@@ -34,8 +34,8 @@ function init() {
 }
 
 function changementFond(temp){
-    var fond = document.getElementById('fond');
-    console.log(fond);
+    var fond = document.getElementById('fond'); //On recupere l'element d'ID 'fond' et on changera son attribut
+    //console.log(fond); //Debugage
     if(temp>25){    //Cas : Température superieur à 25°C
         fond.setAttribute("class","chaud");
     }
@@ -47,24 +47,29 @@ function changementFond(temp){
     else if(temp<10){   //Cas : Température inférieur à 10°C
        fond.setAttribute('class', 'froid');
     }
+    afficherTemp(temp);
 }
 
 function affichageVille(ville){
-    document.getElementById('ville').innerText=ville;
+    document.getElementById('ville').innerText=ville;   //On change le contenu de l'element HTML d'ID 'ville' en y mettant la nouvelle ville
 }
 
 function affichageDate(date){
-    document.getElementById('date').innerText=date;
+    document.getElementById('date').innerText=date;     //On change le contenu de l'elemet HTML d'ID 'date' en y mettant la date des données
 }
 
 function changementVariables(donnes){
     var vitesse_vent = donnes.wind_spd;
     vitesse_vent = vitesse_vent * 3.6; // On transforme les m/s en km/h
     vitesse_vent = Math.round(vitesse_vent); // Arrondi la vitesse du vent
-    document.getElementById('vent-vitesse').innerText = vitesse_vent + " km/h";
-    document.getElementById('soleil-leve').innerText = donnes.sunrise;
-    document.getElementById('soleil-couche').innerText= donnes.sunset;
-    document.getElementById('pluie').innerText = donnes.precip + " mm";
+    document.getElementById('vent-vitesse').innerText = vitesse_vent + " km/h"; //On change le contenu de l'elemet HTML d'ID 'vent-vitesse' et on y met la vitesse du vent
+    document.getElementById('soleil-leve').innerText = donnes.sunrise;          //On change le contenu de l'elemet HTML d'ID 'soleil-leve' et on y met l'heure du lever du soleil
+    document.getElementById('soleil-couche').innerText = donnes.sunset;         //On change le contenu de l'elemet HTML d'ID 'soleil-couche' et on y met l'heure du coucher du soleil
+    document.getElementById('pluie').innerText = donnes.precip + " mm";         //On change le contenu de l'elemet HTML d'ID 'pluie' et on y met la hauteur de pluie tombée
 
 
+}
+
+function afficherTemp(temp){
+    document.getElementById('temperature').innerText = temp + " °C";             //On change le contenu de l'elemet HTML d'ID 'temperature' et on y met la temperature en °C
 }
